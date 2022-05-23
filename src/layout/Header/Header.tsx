819e9ed85f9fe0e71f418184fd8e4b7e19bde366
Billy Chui <billy2022002@gmail.com>
2022-05-23 19:49:12 +1000

    change fixed to sticky

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import { color } from '@/styles/variables';
 
 const StyledHeader = styled.header`
-	position: fixed;
+	position: sticky;
 	top: 0;
 	border-bottom: 1px solid ${color.borderColor};
 	padding-left: 150px;
